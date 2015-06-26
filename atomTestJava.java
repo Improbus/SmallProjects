457016d0bd10e081664be4015d2bfbd2c214acaa
Improbus <daniel.decamillo@gmail.com>
2015-06-25 21:50:10 -0400

    Adding atomTestJava Changes

--- atomTestJava.java
+++ atomTestJava.java
@@ -1,7 +1,11 @@
 class test{
 
   public static void main(String[] args) {
-        System.out.println("Hello World!"+ args[0] + args[1]); // Display the string.
+      System.out.println("Hello World!"+ args[0] + args[1] + addStuff(1,2)); // Display the string.
     }
+
+  public static int addStuff(int x, int y){
+    return x+y;
+  }
 
 }
