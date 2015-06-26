4635a97f46f7265c1e1fa5ee45edf873def9f0c1
Improbus <daniel.decamillo@gmail.com>
2015-06-26 08:55:57 -0400

    Changes

--- atomTestJava.java
+++ atomTestJava.java
@@ -1,11 +1,20 @@
 class test{
 
   public static void main(String[] args) {
-      System.out.println("Hello World!"+ args[0] + args[1] + addStuff(1,2)); // Display the string.
+      System.out.println("Hello World!" + " " + args[0] + " " +  args[1] + " " +  addStuff(1,2) + " " + createSentence()); // Display the string.
+      iAmAVoid();
     }
 
   public static int addStuff(int x, int y){
     return x+y;
+  }
+
+  public static String createSentence(){
+    return "I am a String";
+  }
+
+  public static void iAmAVoid(){
+    System.out.println("I AM A VOID");
   }
 
 }
